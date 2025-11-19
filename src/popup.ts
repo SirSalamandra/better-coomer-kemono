b92f320f13551ee411df6e3dc75032eb96be4a45
SirSalamandra <zxjoaozx@gmail.com>
2025-11-18 22:29:09 -0300

    starting from 0

--- src/popup.ts
+++
@@ -1,8 +0,0 @@
-document.getElementById("clear-urls").addEventListener("click", () => {
-
-  console.log("clear urls")
-
-  chrome.storage.local.set({ artists: [] }, () => {
-    alert("Visited URLs cleared!");
-  });
-});
